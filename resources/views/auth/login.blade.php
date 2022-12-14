2a2c39e72249988ad5281a22849d8264ff582db3
Rizvi <rizvisharis@gmail.com>
2022-12-14 09:08:30 +0530

    add the register link to login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,8 +39,13 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+            <div class="flex items-center justify-end mt-4 gap-2">
+            @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Dont have a account?') }}
+                </a>
+            @endif
+            @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
