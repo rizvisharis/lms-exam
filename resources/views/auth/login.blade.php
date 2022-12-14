6fed0538dc91087d287fc1329d3646548cab940c
Rizvi <rizvisharis@gmail.com>
2022-12-14 09:14:14 +0530

    add alignment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4 gap-2">
+            <div class="flex items-center justify-between mt-4 gap-2">
             @if (Route::has('register'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                     {{ __('Dont have a account?') }}
