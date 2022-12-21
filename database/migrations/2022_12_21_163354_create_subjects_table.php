e6a900ffce1835980a7b74de8237e00e0826cda9
Rizvi <rizvisharis@gmail.com>
2022-12-21 23:12:36 +0530

    implement the subjects and student table migration

---
+++ database/migrations/2022_12_21_163354_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->boolean('exam_availability');
+            $table->dateTime('exam_start');
+            $table->dateTime('exam_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
